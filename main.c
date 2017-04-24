24ab203b3dbe860d1862faee15eff3167c1718ba
Nuura <Nuura@users.noreply.github.com>
2017-04-24 16:57:28 +0200

    Generation de la map pour i >= 10

--- main.c
+++ main.c
@@ -1,4 +1,16 @@
+/*
+** main.c for  in /home/carra/Desktop/My_Snake/trunk
+** 
+** Made by SANCHEZ Pierre
+** Login   <sanche_p@etna-alternance.net>
+** 
+** Started on  Mon Apr 24 15:29:07 2017 SANCHEZ Pierre
+** Last update Mon Apr 24 15:48:12 2017 SANCHEZ Pierre
+*/
+
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 void    read_map(int argc, char *argv[])
 {
@@ -6,7 +18,7 @@ void    read_map(int argc, char *argv[])
   file = fopen(argv[1], "r");
   if (file)
     {
-      printf("tebi en I");
+      printf("tebi en I\n");
       /*file exists and can be opened
       ...
       close file when you're done*/
@@ -14,17 +26,41 @@ void    read_map(int argc, char *argv[])
     }
   else
     {
-      printf("tebi en A");
+      printf("tebi en A\n");
       /*file doesn't exists or cannot be opened (es. you don't have access permission )*/
     }
 }
 
+void generate_map(int argc, char *argv[])
+{
+  if (isdigit(*argv[1]) && isdigit(*argv[2])) {
+    int ref_l = atoi(*argv[1]) -1;
+    int ref_h = atoi(*argv[2]) - 1;
+    int l = ref_l;
+    int h = ref_h;
+    while (h >= 0) {
+      while (l >= 0) {
+	if (l == 0 || l == ref_l || h == 0 || h == ref_h)
+	  printf("X");
+	else
+	  printf(" ");
+	l--;
+      }
+      printf("\n");
+      l = ref_l;
+      h--;
+    }
+  }
+}
+
 void	check_arg(int argc, char *argv[])
 {
-  if (argc < 2)
+  if (argc < 2 || argc > 3)
     printf("Veuillez indiquer le chemin d'une map, ou indiquer sa taille en paramètres\n");
-  if (argc == 2)
+  else if (argc == 2)
     read_map(argc, argv);
+  else if (argc == 3)
+    generate_map(argc, argv);
 }
 
 int	main(int argc, char *argv[])
---
+++ snake.h
@@ -0,0 +1,9 @@
+/*
+** snake.h for  in /home/carra/Desktop/My_Snake/trunk
+** 
+** Made by SANCHEZ Pierre
+** Login   <sanche_p@etna-alternance.net>
+** 
+** Started on  Mon Apr 24 15:29:54 2017 SANCHEZ Pierre
+** Last update Mon Apr 24 15:29:56 2017 SANCHEZ Pierre
+*/
